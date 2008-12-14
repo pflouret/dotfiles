9468c7ab5eb1475ecf8f630a6e05fa212c050676
pablo flouret <palbof@gmail.com>
2008-12-14 06:06:00 +0100

    cron auto-commit

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -15,7 +15,6 @@ import XMonad.Hooks.DynamicLog hiding (shorten)
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.Gaps
--- import XMonad.Layout.HintedTile
 import XMonad.Layout.Circle
 import XMonad.Layout.LayoutCombinators
 import XMonad.Layout.LayoutHints
@@ -23,8 +22,8 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Tabbed
+import XMonad.Layout.TwoPane   
 import XMonad.Layout.WorkspaceDir
--- import XMonad.Layout.WindowNavigation
 import XMonad.ManageHook
 import XMonad.Prompt
 import XMonad.Prompt.XMonad
@@ -33,6 +32,8 @@ import XMonad.Prompt.Window
 import XMonad.Prompt.Workspace
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
+-- import XMonad.Layout.Grid    
+-- import XMonad.Layout.ShowWName   
 
 import Graphics.X11
 import System.Exit
@@ -40,35 +41,53 @@ import System.IO
  
 import qualified Data.Map        as M
 import qualified XMonad.StackSet as W
+
  
 myTerminal      = "urxvtc"
-myBorderWidth   = 1
+myBorderWidth   = 2
 myModMask       = mod4Mask
-myWorkspaces = ["1:term", "2:web", "3:im", "4:arr" ] ++ map show [5..9]
+myWorkspaces = ["1:web", "2:im", "3:term", ":4", ":5", ":6", ":7", "8:music", "9:arr"] -- ++ map show [5..9]
 myNormalBorderColor  = "#444444"
-myFocusedBorderColor = "#ffff00"
+myFocusedBorderColor = "#ff0000"
 
-myTabConfig = defaultTheme {
-    activeColor = "#333333",
-    inactiveColor = "#111111",
-    urgentColor = "#ff0000",
-    activeBorderColor = "#111111",
-    inactiveBorderColor = "#111111",
-    urgentBorderColor = "#00ffff",
-    activeTextColor = "#ffff00",
-    inactiveTextColor = "#aaaaaa",
-    urgentTextColor = "#000000",
-    fontName = "-*-nu-*-*-*-*-*-*-*-*-*-*-*-*",
-    decoHeight = 14
-}
+myXPConfig = defaultXPConfig
+    { font = "-*-terminus-*-*-*-*-*-*-*-*-*-*-*-*"
+    , bgColor = "#111111"
+    , fgColor = "DodgerBlue1"
+    , fgHLight = "#111111"
+    , bgHLight = "DodgerBlue2"
+    -- , borderColor = "#151515"
+    , promptBorderWidth = 0
+    , position = Top 
+    , height = 12
+    , historySize = 200
+    }
+
+myTabConfig = defaultTheme
+    { activeColor = "#333333"
+    , inactiveColor = "#111111"
+    , urgentColor = "#ff0000"
+    , activeBorderColor = "#111111"
+    , inactiveBorderColor = "#111111"
+    , urgentBorderColor = "#00ffff"
+    , activeTextColor = "#ffff00"
+    , inactiveTextColor = "#aaaaaa"
+    , urgentTextColor = "#000000"
+    , fontName = "-*-nu-*-*-*-*-*-*-*-*-*-*-*-*"
+    , decoHeight = 14
+    }
 
 -- myLayout = avoidStruts (gaps [(U,0),(D,16),(L,0),(R,0)] $ tiled ||| Mirror tiled ||| Full)
 myLayout = avoidStruts $ 
            gaps [(U,0),(D,16),(L,0),(R,0)] $ 
            workspaceDir "~" $
            smartBorders $
-           onWorkspace "2:web" (myTabbed ||| Mirror tiled ||| tiled) $
-           tiled ||| Mirror tiled ||| noBorders myTabbed
+           onWorkspaces ["1:web", "8:music"] (myTabbed ||| Mirror tiled ||| tiled) $
+           tiled ||| TwoPane (3/100) (1/2) ||| Mirror tiled ||| noBorders myTabbed
+           -- dragPane Vertical 0.1 0.5
+           -- TwoPane (3/100) (1/2)
+           -- Grid
+           -- magnifier <blah>
   where
      tiled    = ResizableTall nmaster delta ratio [] -- default tiling algorithm partitions the screen into two panes
      myTabbed = tabbed shrinkText myTabConfig
@@ -76,10 +95,6 @@ myLayout = avoidStruts $
      ratio    = 1/2 -- Default proportion of screen occupied by master pane
      delta    = 1/100 -- Percent of screen to increment by when resizing panes
  
---[ ("M-S-<Return>", spawn $ terminal c)
---, ("M-x w", spawn "xmessage 'woohoo!'")  -- type mod+x then w to pop up 'woohoo!'
---, ("M-x y", spawn "xmessage 'yay!'")     -- type mod+x then y to pop up 'yay!'
---, ("M-S-c", kill)
 myMediaKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     [ ((0, 0x1008ff11), spawn "amixer -q sset PCM 12-") -- "aumix -v -2") -- XF86AudioLowerVolume
     , ((0, 0x1008ff12), spawn "amixer -q sset Master toggle ") -- "amixer -q set PCM toggle") -- XF86AudioMute
@@ -96,12 +111,22 @@ myMediaKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 
 myKeys = \conf -> mkKeymap conf $
     [ ("M-r",             spawn $ XMonad.terminal conf) -- launch a terminal
-    , ("M-t",             spawn $ XMonad.terminal conf ++ " -e screen -RR") -- attach to some screen
-    , ("M-S-t",           spawn $ XMonad.terminal conf ++ " -e screen") -- fresh screen
+    , ("M-d",             spawn $ XMonad.terminal conf ++ " -e screen -RR") -- attach to some screen
+    , ("M-S-d",           spawn $ XMonad.terminal conf ++ " -e screen") -- fresh screen
     , ("M-e",             spawn $ XMonad.terminal conf ++ " -e lfm") -- attach to some screen
     , ("C-M1-l",          spawn "xscreensaver-command -lock") -- lock the screen
-    , ("M-<Sys_Req>",     spawn "gscreenshot")
-    , ("<Sys_Req>",       spawn "scrot")
+    , ("C-<Sys_Req>",     spawn "gscreenshot")
+    , ("M-<Sys_Req>",     spawn $ XMonad.terminal conf ++ "scrot -e 'mv $f ~/crap/img/screenshots")
+    , ("<Sys_Req>",       spawn $ XMonad.terminal conf ++ "scrot -c -d 5 -e 'mv $f ~/crap/img/screenshots")
+    , ("M-<Home>",        spawn $ "foobar2000 -playpause")
+    , ("M-<Insert>",      spawn $ "foobar2000 -rand")
+    , ("M-<End>",         spawn $ "foobar2000 -stop")
+    , ("M-<Page_Up>",     spawn $ "foobar2000 -prev")
+    , ("M-<Page_Down>",   spawn $ "foobar2000 -next")
+    , ("M-<Left>",        spawn $ "foobar2000 -command-'Seek back by 5 seconds'")
+    , ("M-<Right>",       spawn $ "foobar2000 -command-'Seek ahead by 5 seconds'")
+    , ("M-<Up>",          spawn $ "foobar2000 -volup")
+    , ("M-<Down>",        spawn $ "foobar2000 -voldown")
     --
     , ("M-<Space>",       sendMessage NextLayout) -- Rotate through the available layout algorithms
     , ("M-S-<Space>",     setLayout $ XMonad.layoutHook conf) --  Reset the layouts on the current workspace to default
@@ -119,20 +144,25 @@ myKeys = \conf -> mkKeymap conf $
     , ("M-o",             moveTo Next NonEmptyWS)
     , ("M-i",             moveTo Prev NonEmptyWS)
     --
-    , ("M1-<Tab>",        windows W.focusDown) -- Move focus to the next window
-    , ("M1-S-<Tab>",      windows W.focusUp) -- Swap the focused window with the next window
-    , ("M-m",             windows W.focusMaster) -- Move focus to the master window
-    , ("M-<Return>",      windows W.swapMaster) -- Swap the focused window and the master window
-    , ("M-S-j",           windows W.swapDown) -- Swap the focused window with the next window
-    , ("M-S-k",           windows W.swapUp) -- Swap the focused window with the previous window
-    , ("M-<Backspace>",   focusUrgent) -- Move focus to the master window
+    , ("M1-<Tab>",        windows W.focusDown)
+    , ("M1-S-<Tab>",      windows W.focusUp)
+    , ("M-j",             windows W.focusDown)
+    , ("M-k",             windows W.focusUp)
+    , ("M-h",             windows W.focusUp)
+    , ("M-l",             windows W.focusDown)
+
+    , ("M-m",             windows W.focusMaster)
+    , ("M-<Return>",      windows W.swapMaster)
+    , ("M-S-j",           windows W.swapDown)
+    , ("M-S-k",           windows W.swapUp)
+    , ("M-<Backspace>",   focusUrgent)
     -- move/resize floats
     , ("M-C-h",           withFocused $ keysMoveWindow (-10,0))
     , ("M-C-j",           withFocused $ keysMoveWindow (0,10))
     , ("M-C-k",           withFocused $ keysMoveWindow (0,-10))
     , ("M-C-l",           withFocused $ keysMoveWindow (10,0))
-    , ("M-M1-C-k",        withFocused $ keysResizeWindow (10,10) (1,1))
-    , ("M-M1-C-j",        withFocused $ keysResizeWindow (-10,-10) (1,1))
+    , ("M-M1-C-k",        withFocused $ keysResizeWindow (10,10) (0,0))
+    , ("M-M1-C-j",        withFocused $ keysResizeWindow (-10,-10) (0,0))
     --
     , ("M-s",             withFocused $ windows . W.sink)
     , ("M-M1-h",          sendMessage Shrink) -- Shrink the master area
@@ -162,9 +192,7 @@ myKeys = \conf -> mkKeymap conf $
     --    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
  
  
-------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
---
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
  
     -- mod-button1, Set the window to floating mode and move by dragging
@@ -179,25 +207,11 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
  
-------------------------------------------------------------------------
 -- Window rules:
- 
--- Execute arbitrary actions and WindowSet manipulations when managing
--- a new window. You can use this to, for example, always float a
--- particular program, or have a client always appear on a particular
--- workspace.
---
--- To find the property name associated with a program, use
--- > xprop | grep WM_CLASS
--- and click on the client you're interested in.
---
--- To match on the WM_NAME, you can use 'title' in the same way that
--- 'className' and 'resource' are used below.
---
 myManageHook = composeAll
     [ className =? "MPlayer1"       --> doFloat
     , className =? "Gimp"           --> doFloat
-    -- , title     =? "VLC media player" --> doFloat
+    , className =? "Gscreenshot.py" --> doFloat
     , resource  =? "volwheel"       --> doFloat
     , resource  =? "xfontsel"       --> doFloat
     , className =? "Conky"          --> doIgnore
@@ -205,28 +219,16 @@ myManageHook = composeAll
     , resource  =? "stalonetray"    --> doIgnore
     , resource  =? "desktop_window" --> doIgnore
     , resource  =? "kdesktop"       --> doIgnore
-    , className =? "Opera"          --> doF (W.shift "2:web") 
+    , className =? "Opera"          --> doF (W.shift "1:web") 
+    , className =? "Gajim.py"       --> doF (W.shift "2:im") 
+    , resource  =? "foobar2000.exe" --> doF (W.shift "8:music") 
+    -- , title     =? "VLC media player" --> doFloat
     ] -- <+> manageDocks
 
--- Urgency hint options:
 myUrgencyHook = withUrgencyHook NoUrgencyHook
 -- myUrgencyHook = withUrgencyHook dzenUrgencyHook
 --     { args = ["-x", "0", "-y", "1184", "-h", "16", "-w", "1600", "-ta", "r", "-expand", "l", "-fg", "#0099ff", "-bg", "#0f0f0f", "-fn", "-xos4-terminus-medium-r-normal-*-14-*-*-*-c-*-iso10646-1"] }
  
--- XPConfig options:
-myXPConfig = defaultXPConfig
-    { font = "-*-terminus-*-*-*-*-*-*-*-*-*-*-*-*"
-    , bgColor = "#111111"
-    , fgColor = "#00ff00"
-    , fgHLight = "#111111"
-    , bgHLight = "#00ff00"
-    , borderColor = "#111111"
-    , promptBorderWidth = 1
-    , position = Top 
-    , height = 12
-    , historySize = 200
-    }
-
 shorten :: Int -> String -> String
 shorten n xs | length xs < n = xs
              | otherwise     = (take (n - length end) xs) ++ end
@@ -257,43 +259,11 @@ myDzenPP h = defaultPP
     , ppTitle   = dzenColor "#bbbbbb" "" . wrap "< " " >" 
     , ppOutput   = hPutStrLn h
     }
-{-
-myLogHook h = do
-  dynamicLogWithPP $ xmobPP h 
-  
-xmobPP :: Handle -> PP
-xmobPP h = defaultPP  { ppCurrent = wrap "<fc=black,yellow3> " " </fc>" 
-                     , ppSep     = ""
-                     , ppWsSep = ":"
-                     , ppVisible = wrap "<fc=black,DarkSlateGray4> " " </fc>" 
-                     , ppTitle = \x -> case length x of
-                                         0 -> ""
-                                         _ -> "<fc=green2,black>" ++ shorten 83 x ++ "</fc>"
-                     , ppLayout = \x -> "<fc=yellow2,black> | "
-                                  ++ case x of
-                                       "Tall"                   -> "tiled"
-                                       "Mirror ResizableTall"   -> "mtiled"
-                                       "ResizableTall"          -> "tiled"
-                                       "Full"                   -> "full"
-                                       "Tabbed Bottom Simplest" -> "tabbed"
-                                       "Tabbed Simplest"        -> "tabbed"
-                                       _                        -> x
-                                  ++ " ::</fc> "
-                     , ppHidden = wrap " " " "
-                     , ppUrgent = wrap "<fc=black,red>" "</fc>"
-                     -- , ppOrder  = reverse
-                     , ppOutput = hPutStrLn h
-                     }
--}
  
-------------------------------------------------------------------------
 -- Startup hook
- 
 -- Perform an arbitrary action each time xmonad starts or is restarted
 -- with mod-q.  Used by, e.g., XMonad.Layout.PerWorkspace to initialize
 -- per-workspace layout choices.
---
--- By default, do nothing.
 myStartupHook = return ()
  
 -- The mask for the numlock key. Numlock status is "masked" from the
@@ -313,22 +283,24 @@ myNumlockMask   = mod2Mask
  
 main = do
     din <- spawnPipe myStatusBar
-    config <- withWindowNavigation (xK_k, xK_h, xK_j, xK_l)
-            $ myUrgencyHook
-            $ defaultConfig
-            { terminal           = myTerminal
-            , focusFollowsMouse  = False
-            , borderWidth        = myBorderWidth
-            , modMask            = myModMask
-            , numlockMask        = myNumlockMask
-            , workspaces         = myWorkspaces
-            , normalBorderColor  = myNormalBorderColor
-            , focusedBorderColor = myFocusedBorderColor
-            , keys               = \c -> M.union (myKeys c) (myMediaKeys c)
-            , mouseBindings      = myMouseBindings
-            , layoutHook         = myLayout
-            , manageHook         = myManageHook
-            , logHook            = dynamicLogWithPP $ myDzenPP din
-            , startupHook        = myStartupHook
-            }
-    xmonad config
+    xmonad $ myUrgencyHook
+           $ defaultConfig
+           { terminal           = myTerminal
+           , focusFollowsMouse  = False
+           , borderWidth        = myBorderWidth
+           , modMask            = myModMask
+           , numlockMask        = myNumlockMask
+           , workspaces         = myWorkspaces
+           , normalBorderColor  = myNormalBorderColor
+           , focusedBorderColor = myFocusedBorderColor
+           , keys               = \c -> M.union (myKeys c) (myMediaKeys c)
+           , mouseBindings      = myMouseBindings
+           , layoutHook         = myLayout
+           -- , layoutHook         = showWName myLayout
+           , manageHook         = myManageHook
+           , logHook            = dynamicLogWithPP $ myDzenPP din
+           , startupHook        = myStartupHook
+           }
+    -- <- withWindowNavigation (xK_k, xK_h, xK_j, xK_l)
+    -- xmonad config
+
