39fdec47dab2360fbd4c064be29bc5a18573bfb6
pablo flouret <palbof@gmail.com>
2009-01-08 06:06:00 +0100

    cron auto-commit

--- .zsh.d/style.zsh
+++ .zsh.d/style.zsh
@@ -1,5 +1,5 @@
 #zstyle ':completion:*' completer _expand _complete _ignored _match _approximate _prefix
-zstyle ':completion:*' completer _complete _expand _prefix
+zstyle ':completion:*' completer _complete _expand _prefix _gnu_generic
 zstyle ':completion:*' ignore-parents pwd directory
 zstyle ':completion:*' insert-unambiguous true
 zstyle ':completion:*' list-colors ''
@@ -7,8 +7,12 @@ zstyle ':completion:*' list-suffixes true
 zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*'
 zstyle ':completion:*' original true
 zstyle ':completion:*' preserve-prefix '//[^/]##/'
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.zshcache
 #zstyle ':completion:*' menu yes select
 zstyle :compinstall filename "$HOME/.zshrc"
+
+FPATH=~/.zsh.d/completion:$FPATH
 
 autoload -Uz compinit
 compinit
@@ -19,3 +23,5 @@ compdef pkill=killall
 zstyle ':completion:*:kill:*' menu yes select
 zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,args --sort cmd,args'
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31' 
+
+zstyle ':completion:*:xmms2:*' menu yes select
