15ef4ecd997833c306314ba2dedacb8feffdcb3f
pablo flouret <palbof@gmail.com>
2009-01-22 06:06:00 +0100

    cron auto-commit

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -128,7 +128,7 @@ myKeys = \conf -> mkKeymap conf $
     [ ("M-r",             spawn $ XMonad.terminal conf) -- launch a terminal
     , ("M-d",             spawn $ XMonad.terminal conf ++ " -e screen -RR") -- attach to some screen
     , ("M-S-d",           spawn $ XMonad.terminal conf ++ " -e screen") -- fresh screen
-    , ("M-e",             spawn $ XMonad.terminal conf ++ " -e mc -x")
+    , ("M-e",             spawn $ XMonad.terminal conf ++ " -e vifm")
     , ("M-S-e",           spawn "thunar")
     , ("M-t",             spawn $ XMonad.terminal conf ++ " -e htop")
     , ("M-a",             spawn "xscreensaver-command -lock") -- lock the screen
