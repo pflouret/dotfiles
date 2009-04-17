295e684076f52e1728bdaa12171afc2200339076
pablo flouret <quuxbaz@gmail.com>
2009-04-17 06:06:00 +0200

    cron auto-commit

--- .conky/bin/bat_time_left.sh
+++ .conky/bin/bat_time_left.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-t=`acpi|cut -d " " -f 10`
+t=`acpi|cut -d " " -f 5`
 
 [ -z $t ] && exit 0
 
 hour=${t:0:2}
 minute=${t:3:2}
 
-if ! `acpi|grep -q discharging`; then
+if ! `acpi|grep -qi discharging`; then
   echo -n 'AC '
 fi
 [ "$hour" != '00' ] && echo -n "${hour}h "
