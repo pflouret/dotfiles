ea2a17cff5aa6041f7c7dfb70a2bae2a8bb96da8
Pablo Flouret <pflouret@pandora.com>
2014-04-13 15:42:57 -0700

    omgomgomg.

--- .zsh.d/alias.zsh
+++ .zsh.d/alias.zsh
@@ -44,7 +44,7 @@ alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
 alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
-alias omg="say -r 350 -v trinoids omg omg omg"
+alias omg="say -r 350 -v trinoids omg omg omg; terminal-notifier -message 'omg omg omg'"
 
 dum() {
   n=$1
