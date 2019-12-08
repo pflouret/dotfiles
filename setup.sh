6ebfd2afa9e3ce3675f7f770b7e9116083230c1d
Pablo Flouret <pflouret@gmail.com>
2019-12-08 01:24:10 -0800

    cleanup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ddir=`dirname $0`
-files=`find $ddir -maxdepth 1 ! -path $ddir ! -path $ddir/.gitignore ! -path $ddir/.git ! -path '*.sh'`
+files=`find $ddir -maxdepth 1 ! -path $ddir ! -path $ddir/old ! -path $ddir/.gitignore ! -path $ddir/.git ! -path '*.sh'` ! -path Brewfile
 
 mkdir dotfiles-bkp
 for f in $files; do
